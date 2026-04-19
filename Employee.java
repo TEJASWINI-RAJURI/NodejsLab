d5fbe7f8a89482b1b7a4a57aa15badd3376139cf
TEJASWINI-RAJURI <tejaswiniiiii14@gmail.com>
2026-04-19 23:00:19 +0530

    Create Employee.java

---
+++ Employee.java
@@ -0,0 +1,43 @@
+
+package cse5c8;
+
+public class Employee {
+
+private int eid;
+private String ename;
+private int salary;
+private int age;
+
+public int getEid() {
+return eid;
+}
+
+public void setEid(int eid) {
+this.eid = eid;
+}
+
+public String getEname() {
+return ename;
+}
+
+public void setEname(String ename) {
+this.ename = ename;
+}
+
+public int getSalary() {
+return salary;
+}
+
+public void setSalary(int salary) {
+this.salary = salary;
+}
+
+public int getAge() {
+return age;
+}
+
+public void setAge(int age) {
+this.age = age;
+}
+
+}
